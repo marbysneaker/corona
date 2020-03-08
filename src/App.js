85d4077474c50d39dacffcd0722e1105ec5a97c2
marby <marbysneaker@gmail.com>
2020-03-07 16:54:56 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import ReactNbsp from 'react-nbsp'
 
 class App extends Component {
 state = {
@@ -49,7 +49,7 @@ render() {
       <header className="App-header">
         <div className = "corona-container">
             <div className = "world">
-              WORLD
+              <div className='world-title'>WORLD </div> 
               <div className='world-cases'>CASES : {this.state.world.cases} </div>
               <div className='world-deaths'>DEATHS : {this.state.world.deaths}</div>
               <div className='world-recovered'>RECOVERED : {this.state.world.recovered}</div>
@@ -58,19 +58,19 @@ render() {
             </div>
             <div className = "usa">
                 <div className='country'>COUNTRY : {this.state.usa.country}</div>
-                <div className='cases'>CASES : &nbsp &nbsp {this.state.usa.cases}</div>
-                <div className='today'>TODAY'S CASES : &nbsp &nbsp  {this.state.usa.todayCases}</div>
-                <div className='deaths'>TODAY'S DEATHS : &nbsp&nbsp {this.state.usa.todayDeaths}</div>
-                <div className='recovered'>TODAY'S RECOVERED : &nbsp &nbsp {this.state.usa.recovered}</div>
-                <div className='critical'>CRITICAL : -&nbsp &nbsp {this.state.usa.critical}</div>
+                <div className='cases'>CASES : <ReactNbsp/> <ReactNbsp/> {this.state.usa.cases}</div>
+                <div className='today'>TODAY'S CASES :   {this.state.usa.todayCases}</div>
+                <div className='deaths'>TODAY'S DEATHS :  {this.state.usa.todayDeaths}</div>
+                <div className='recovered'>TODAY'S RECOVERED :  {this.state.usa.recovered}</div>
+                <div className='critical'>CRITICAL :   {this.state.usa.critical}</div>
             </div>
             <div className = "pi">
                 <div className='country'>COUNTRY :{this.state.philippines.country}</div>
                 <div className='cases'>CASES :{this.state.philippines.cases}</div>
-                <div className='today'>{this.state.philippines.todayCases}</div>
-                <div className='deaths'>{this.state.philippines.todayDeaths}</div>
-                <div className='recovered'>{this.state.philippines.recovered}</div>
-                <div className='critical'>{this.state.philippines.critical}</div>
+                <div className='today'>TODAY'S CASES :{this.state.philippines.todayCases}</div>
+                <div className='deaths'>TODAY'S DEATHS :{this.state.philippines.todayDeaths}</div>
+                <div className='recovered'>RECOVERED :{this.state.philippines.recovered}</div>
+                <div className='critical'>CRITICAL :{this.state.philippines.critical}</div>
             </div>
             <div className = "all">
             4
