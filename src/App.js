f248d8ffcb1a0dc0424a83720eb7feef67d42823
marby <marbysneaker@gmail.com>
2020-03-11 14:52:03 -0700

    added css

--- src/App.js
+++ src/App.js
@@ -26,9 +26,8 @@ doFetch = () => {
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const url1 = "https://corona.lmao.ninja/all"
   const url2 = "https://corona.lmao.ninja/countries"
-  var covid = require('novelcovid');
 
-  fetch(proxyUrl + url1)
+  fetch(url1)
       .then(response => response.json())
       .then(data => {
       console.log(data)
@@ -36,7 +35,7 @@ doFetch = () => {
       world: data
       })})
     .catch((err) => console.error(err));
-  fetch(proxyUrl + url2)
+  fetch(url2)
       .then(response => response.json())
       .then(data => {
       console.log('received twitter data', data)
@@ -100,29 +99,29 @@ render() {
         <div className = "corona-container">
             <div className = "world">
               <div className='world-title'>WORLD </div> 
-              <div className='world-cases'>CASES : {this.state.world.cases} </div>
-              <div className='world-deaths'>DEATHS : {this.state.world.deaths}</div>
-              <div className='world-recovered'>RECOVERED : {this.state.world.recovered}</div>
+              <div className='world-cases'>CASES <br/> <span>{this.state.world.cases} </span></div>
+              <div className='world-deaths'>DEATHS <br/> <span>{this.state.world.deaths}</span></div>
+              <div className='world-recovered'>RECOVERED <br/><span>{this.state.world.recovered}</span></div>
               
             
             </div>
             <div className = "usa">
                 <div className='country-usa'>{this.state.usa.country}</div>
-                <div className='cases'>CASES : <ReactNbsp/> <ReactNbsp/> {this.state.usa.cases}</div>
-                <div className='today'>TODAY'S CASES :   {this.state.usa.todayCases}</div>
-                <div className='todays-deaths'>TODAY'S DEATHS :  {this.state.usa.todayDeaths}</div>
-                <div className='recovered'>TODAY'S RECOVERED :  {this.state.usa.recovered}</div>
-                <div className='critical'>CRITICAL :   {this.state.usa.critical}</div>
-                <div className='deaths'>DEATHS : {this.state.usa.deaths}</div>
+                <div className='cases'>CASES <br/> <span>{this.state.usa.cases}</span></div>
+                <div className='today'>TODAY'S CASES <br/><span>{this.state.usa.todayCases}</span> </div>
+                <div className='todays-deaths'>TODAY'S DEATHS <br/><span>{this.state.usa.todayDeaths}</span></div>
+                <div className='recovered'>TODAY'S RECOVERED <br/> <span>{this.state.usa.recovered}</span></div>
+                <div className='critical'>CRITICAL <br/> <span>{this.state.usa.critical}</span></div>
+                <div className='deaths'>DEATHS <br/><span>{this.state.usa.deaths}</span> </div>
             </div>
             <div className = "pi">
                 <div className='country-pi'>{this.state.philippines.country}</div>
-                <div className='cases'>CASES :{this.state.philippines.cases}</div>
-                <div className='today'>TODAY'S CASES :{this.state.philippines.todayCases}</div>
-                <div className='todays-deaths'>TODAY'S DEATHS :{this.state.philippines.todayDeaths}</div>
-                <div className='recovered'>RECOVERED :<span className= 'recovered-info'>{this.state.philippines.recovered}</span></div>
-                <div className='critical'>CRITICAL : <span className= 'critical-info'>{this.state.philippines.critical}</span></div>
-                <div className='deaths'>DEATHS : {this.state.philippines.deaths}</div>
+                <div className='cases'>CASES <br/><span>{this.state.philippines.cases}</span></div>
+                <div className='today'>TODAY'S CASES <br/> <span>{this.state.philippines.todayCases}</span></div>
+                <div className='todays-deaths'>TODAY'S DEATHS <br/><span>{this.state.philippines.todayDeaths}</span></div>
+                <div className='recovered'>RECOVERED <br/><span>{this.state.philippines.recovered}</span></div>
+                <div className='critical'>CRITICAL <br/> <span>{this.state.philippines.critical}</span></div>
+                <div className='deaths'>DEATHS <br/> <span>{this.state.philippines.deaths}</span></div>
             </div>
             <div className='twitter-usa'>
                     <TwitterTimelineEmbed
